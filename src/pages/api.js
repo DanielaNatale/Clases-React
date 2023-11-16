688578493b972cfda6b87168e9cf2dd5a4c85975
Daniela Natale <daniela.natale13@gmail.com>
2023-11-16 10:35:55 +0100

    clase 27

--- src/pages/api.js
+++ src/pages/api.js
@@ -3,40 +3,43 @@ import { useEffect, useState } from 'react'; //Hooks
 
 const Api = (props) => {
 
-    const [cargando, setCargando]= useState(false);
-    const [personajes, setPersonajes]= useState([]);
+    const [cargando, setCargando] = useState(false);
+    const [personajes, setPersonajes] = useState([]);
 
-    useEffect(()=> {
-        const cargarDatos = async() =>{
+    useEffect(() => {
+        const cargarDatos = async () => {
             setCargando(true);
-            const res = (await fetch('https://rickandmortyapi.com/api/character')).json();
+            const res = await (await fetch('https://rickandmortyapi.com/api/character')).json();
             setPersonajes(res.results);
             setCargando(false);
         }
         cargarDatos();
     }, []);
 
-//fetch > para traer los datos nativos de JS. Tambien se puede utilizar  axios(instalar dependencia)
+    //fetch > para traer los datos nativos de JS. Tambien se puede utilizar  axios(instalar dependencia)
 
 
     return (
         <>
             <h2>Datos API</h2>
             {cargando ? <p>Cargando datos...</p> : (
-            <div className="personajes">
-                <div className='personaje'>
-                    {/* <h4>Nombre del personaje</h4> */}
-                    <div className="ficha">
-                        <div className="foto">
-                            <img src="https://fakeimg.pl/150x150?text=img" alt="foto" />
+                <div className="personajes">
+                    {personajes.map(personaje => (
+                        <div className='personaje' key={personaje.id}>
+                            <h4>{personaje.name}</h4>
+                            <div className="ficha">
+                                <div className="foto">
+                                    <img src={personaje.image} alt={personaje.name} />
+                                </div>
+                                <div className="datos">
+                                    <h6> Especie : {personaje.species==="Human" ? 'Humano' : personaje.species} </h6>
+                                    <h6> Vivo : {personaje.status==="Alive" ? 'parece ser' : 'no'} </h6>
+                                </div>
+                            </div>
                         </div>
-                        <div className="datos">
-                            <h6> Especie: Humano o no </h6>
-                            <h6> Vivo: si o no </h6>
-                        </div>
-                    </div>
+                    ))}
                 </div>
-            </div>
+
             )} {/*termina el operador ternario de la lunea 25*/}
 
         </>
